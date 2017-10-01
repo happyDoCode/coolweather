4e8ec75d502adb2036684f60a101837f6b40e31a
刘晓阳 <1445925233@qq.com>
2017-10-01 20:03:12 +0800

    加入数据库和表的各项配配置

---
+++ app/src/main/java/com/lxy/coolweather/db/Province.java
@@ -0,0 +1,38 @@
+package com.lxy.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 刘晓阳 on 2017/10/1.
+ */
+
+public class Province extends DataSupport {
+
+    private int id;
+    private String privinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPrivinceName() {
+        return privinceName;
+    }
+
+    public void setPrivinceName(String privinceName) {
+        this.privinceName = privinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
